bb6f41d4af79db9e878fbee3a48e67839684b13f
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2022-08-22 16:53:43 +0000

    Bare clean up

--- src/utils/getDataFromYoutube.ts
+++ src/utils/getDataFromYoutube.ts
@@ -1,48 +1,54 @@
 export const getPlaylistsFromChannel = async function () {
-  const API_KEY = process.env.API_KEY;
-  const channelId = process.env.YOUTUBE_CHANNEL_ID;
-  const result = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=100&key=${API_KEY}`
-  );
-  const { items } =await result.json();
-  return [...items];
+  try {
+    const API_KEY = process.env.API_KEY;
+    const channelId = process.env.YOUTUBE_CHANNEL_ID;
+    const result = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=100&key=${API_KEY}`
+    );
+    const { items } = await result.json();
+    return [...items];
+  } catch (error) {
+    return [];
+  }
 };
 
 export const getVideoesFromPlaylist = async (id: string) => {
-  // console.log("----------------->",id);
 
-  const API_KEY = process.env.API_KEY;
-  const result = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=100&playlistId=${id}&key=${API_KEY}`
-  );
-  const data =await result.json();
-  return [...data.items];
+  try {
+    const API_KEY = process.env.API_KEY;
+    const result = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=100&playlistId=${id}&key=${API_KEY}`
+    );
+    const data = await result.json();
+    return [...data.items];
+  } catch (error) {
+    return [];
+  }
 };
+
 export const getVideoDetails = async (id: string) => {
-  const API_KEY = process.env.API_KEY;
-  const result = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${API_KEY}`
-  );
-  const { items } =await result.json();
-  return [...items];
+  try {
+    const API_KEY = process.env.API_KEY;
+    const result = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${API_KEY}`
+    );
+    const { items } = await result.json();
+    return [...items];
+  } catch (error) {
+    return [];
+  }
 };
 
 export const getRecentVideoes = async () => {
-  const API_KEY = process.env.API_KEY;
-  const channelId = process.env.YOUTUBE_CHANNEL_ID;
-  const result = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${channelId}&maxResults=25&key=${API_KEY}`
-  );
-  const { items } =await result.json();
-  return [...items];
+  try {
+    const API_KEY = process.env.API_KEY;
+    const channelId = process.env.YOUTUBE_CHANNEL_ID;
+    const result = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${channelId}&maxResults=25&key=${API_KEY}`
+    );
+    const { items } = await result.json();
+    return [...items];
+  } catch (error) {
+    return [];
+  }
 };
-
-/*
-(async function getData() {
-  const YOUR_API_KEY="AIzaSyCiG6rVzN_p17OONMmM_g0CwtnAE9MrvwQ";
-  const result=await fetch(`https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UCS7wp52_Hot7wohyHlSEdag&maxResults=100&key=${YOUR_API_KEY}`);
-  const data=result.json();
-  console.log(data);
-  
-})();
-*/
